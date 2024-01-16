0b40f4e4bf1730d523cf7b76b52e7e25a30c1ad1
Aurelien Simon <aureliensimon@hotmail.com>
2024-01-16 22:02:19 +0100

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Faker::Config.locale = 'fr'
+
+(1..10).each do |restaurant|
+  restaurant = Restaurant.new(name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    category: Restaurant::VALID_CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.phone_number)
+  restaurant.save
+end
